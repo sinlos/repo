59d7ad7a43fbbe6c3b974d89ce0758ebc415db2f
Ilya Pogrebenko <sinlos@gmail.com>
2021-08-03 05:13:23 +0300

    test

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-mvn install:install-file -DgroupId=rcme.mockinizer -DartifactId=ksp -Dversion=1.0-SNAPSHOT -Dfile=processor-1.0-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
-mvn install:install-file -DgroupId=rcme.mockinizer -DartifactId=annotation -Dversion=1.0-SNAPSHOT -Dfile=annotation-1.0-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
-
